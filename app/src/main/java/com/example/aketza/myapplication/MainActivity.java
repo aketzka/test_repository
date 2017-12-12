76903c0f935f8a7eed2d6ac6509d9324458536df
aketza <aketzka@gmail.com>
2017-12-12 10:13:05 +0300

    starting_cheat

--- app/src/main/java/com/example/aketza/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aketza/myapplication/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.aketza.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main(Quiz)Activity";
+    private static final String KEY_INDEX = "index";
     private TextView mQuestionText;
     private Button mTrueButton;
     private Button mFalseButton;
@@ -23,8 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "creation");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null)
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+
 
         mQuestionText = (TextView)findViewById(R.id.question_text);
         mTrueButton = (Button)findViewById(R.id.true_button);
@@ -59,9 +67,46 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG, "saving index in bundle");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStart() {
+        Log.d(TAG, "starting");
+        super.onStart();
+    }
+
+    @Override
+    public void onPause(){
+        Log.d(TAG, "taking pause");
+        super.onPause();
+    }
+
+    @Override
+    public void onResume(){
+        Log.d(TAG, "resuming");
+        super.onResume();
+    }
+
+    @Override
+    public void onStop(){
+        Log.d(TAG, "stopping");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy(){
+        Log.d(TAG, "destroing");
+        super.onDestroy();
+    }
+
+
     private void refreshQuestion(){
         Question question = mQuestionBank[mCurrentIndex];
-        mQuestionText = (TextView)findViewById(R.id.question_text);
         mQuestionText.setText(question.getQuestionText());
     }
 
